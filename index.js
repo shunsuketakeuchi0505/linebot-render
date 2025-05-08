2bf79fa8ab5352e6ea5f4735e0bce7927235b796
shunsuketakeuchi0505 <shunminto0505@gmail.com>
2025-05-08 19:43:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+const app = express();
+
+const config = {
+  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+  channelSecret: 'YOUR_CHANNEL_SECRET'
+};
+
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: `受け取りました：「${event.message.text}」`
+  });
+}
+
+app.listen(3000, () => {
+  console.log('LINE Bot running');
+});
